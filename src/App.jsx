2686cdf7f3d9b35a0aa5b6e4f45b5349601c89df
akhterjawad <akhterjawad566@gmail.com>
2024-12-11 23:08:49 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ function App() {
           placeholder="Add a new Task"
           className="border-[#9E78CF] border-2  rounded-xl  sm:w-[25rem] w-[70%] p-2 outline-none bg-[#0D0714] text-white"
         />
-        <button className="bg-[#9E78CF] ml-2 rounded-lg pl-3 pr-3 pb-2 pt-1 text-white text-[1.5rem]">
-          +
-        </button>
+        <button className="bg-[#9E78CF] ml-2 rounded-lg pl-3 pr-3 pb-2 pt-1 text-white text-[1.5rem]">+</button>
         <p className="text-white mt-5  sm:mr-[23rem] mr-[60%]">Tasks to do</p>
       </div>
       <div></div>
